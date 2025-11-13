d23986b96a9d5b3ba99684c60a0fbfebc338e20d
MahimaGA <mahimaagarwal285@gmail.com>
2025-11-13 18:28:02 +0545

    Check input file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import sys
 import pandas as pd
 
-data_frame = pd.read_excel(sys.argv[1])
-data_frame.to_excel("cleaned_output.xlsx", index=False)
+if len(sys.argv) != 2:
+    print("Please provide one file as argument")
+    sys.exit()
+
+input_file = sys.argv[1]
+
+try:
+    data_frame = pd.read_excel(input_file)
+except ValueError:
+    print("Please provide an excel file")
+    sys.exit()
+
+output_file = f"cleaned_{input_file}"
+data_frame.to_excel(output_file, index=False)
